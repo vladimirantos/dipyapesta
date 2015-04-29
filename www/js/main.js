7435598c4de52f5864851724c815b8fdfd496737
Bruno Puzják <puzjak.bruno@gmail.com>
2015-04-29 17:10:23 +0200

    css pro mobil

--- www/js/main.js
+++ www/js/main.js
@@ -4,7 +4,21 @@ $(document).ready(function () {
         $a = $(this).find("a");
         window.location.href = $a.prop('href');
     });
-
+    $("#menuBtn").click(function (e) {
+        ul = $("#menuUl");
+        nav = $("nav");
+        if (ul.hasClass("active")) {
+            ul.removeClass("active");
+            ul.css("display", "none");
+            nav.css("height", "50px");
+            $(this).css("padding-bottom", "0");
+        } else {
+            ul.addClass("active");
+            ul.css("display", "block");
+            nav.css("height", "260px");
+            $(this).css("padding-bottom", "10px");
+        }
+    });
 
     setTimeout(function ()
     {
