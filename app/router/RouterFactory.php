ae0bbfd8539be4a68c9c4977945111e1e9804b3a
Bruno Puzják <puzjak.bruno@gmail.com>
2015-07-31 18:11:56 +0200

    hezké url pro produkty
    ale je potřeba změnit všechny adresy v latte a id v routeru

--- app/router/RouterFactory.php
+++ app/router/RouterFactory.php
@@ -12,6 +12,12 @@ use Nette,
  */
 class RouterFactory {
 
+    private $products = array(
+        'balsamica' => "208",
+        'dipy' => "203",
+        'pesta' => "202"
+    );
+
     /**
      * @return \Nette\Application\IRouter
      */
@@ -22,8 +28,17 @@ class RouterFactory {
         $admin[] = new Route('admin/<presenter>/<action>[/<id>]', 'Homepage:default');
         $router[] = $admin;
 
-
+        $router[] = new Route('<presenter>/<action>/<id>', array(
+            'presenter' => "products",
+            'action' => "detail",
+            'id' => array(
+                Route::FILTER_TABLE => ($this->products)
+            ),
+        ));
         $router[] = new Route('<presenter>/<action>[/<id>]', 'Homepage:default');
+
+
+
         return $router;
     }
 
